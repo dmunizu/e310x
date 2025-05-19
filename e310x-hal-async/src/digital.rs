51d4121b1860b777e517987d06060426687e7578
Daniel Muñiz <d.muniz@alumnos.upm.es>
2025-05-19 11:36:03 +0200

    digital.rs creation

---
+++ e310x-hal-async/src/digital.rs
@@ -0,0 +1,6 @@
+//! # Digital I/O
+//! # Note
+//!
+//! Implementation of the Async Embedded HAL I/O functionality.
+use embedded_hal_async::digital::{InputPin, OutputPin, StatefulOutputPin};
+use core::task::Poll;
