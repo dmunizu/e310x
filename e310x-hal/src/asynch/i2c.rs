0b8084dd86e1bd0da85273b3f94af8bf51125956
dmunizu <d.muniz@alumnos.upm.es>
2025-06-30 17:01:17 +0200

    Add placeholder for transaction()

--- e310x-hal/src/asynch/i2c.rs
+++ e310x-hal/src/asynch/i2c.rs
@@ -3,3 +3,15 @@
 //!
 //! Implementation of the Async Embedded HAL I2C functionality.
 //!
+use crate::i2c::{I2c, I2cX};
+use embedded_hal_async::i2c;
+
+impl<I2C: I2cX, PINS> i2c::I2c for I2c<I2C, PINS> {
+    async fn transaction(
+        &mut self,
+        address: u8,
+        operations: &mut [i2c::Operation<'_>],
+    ) -> Result<(), Self::Error> {
+        Ok(()) // Placeholder for async transaction implementation
+    }
+}
