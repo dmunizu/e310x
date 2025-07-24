951fae6b34110e2c0dde8fe9ccfdf22613bac759
dmunizu <d.muniz@alumnos.upm.es>
2025-07-24 12:33:32 +0200

    Add Prelude SPI entry and spi.rs mods

--- e310x-hal/src/asynch/prelude.rs
+++ e310x-hal/src/asynch/prelude.rs
@@ -2,3 +2,5 @@
 pub use embedded_hal_async::delay::DelayNs as _eha_DelayNs;
 pub use embedded_hal_async::digital::Wait as _eha_Wait;
 pub use embedded_hal_async::i2c::I2c as _eha_I2c;
+pub use embedded_hal_async::spi::SpiBus as _eha_SpiBus;
+pub use embedded_hal_async::spi::SpiDevice as _eha_SpiDevice;
---
+++ e310x-hal/src/asynch/spi.rs
@@ -0,0 +1,10 @@
+//! # SPI Asynchronous Implementation
+//! # Note
+//!
+//! Implementation of the Async Embedded HAL SPI functionality.
+//!
+
+mod bus; // contains the SPI Bus abstraction
+mod exclusive_device; // contains the exclusive SPI device abstraction
+mod shared_bus; // shared bus newtype
+mod shared_device; // contains the shared SPI device abstraction
