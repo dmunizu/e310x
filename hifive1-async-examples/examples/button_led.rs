f35f75448e3ea35e3513d42d7fb41b97e8ffeebe
dmunizu <d.muniz@alumnos.upm.es>
2025-11-18 10:57:03 +0100

    Make button_led.rs more legible

--- hifive1-async-examples/examples/button_led.rs
+++ hifive1-async-examples/examples/button_led.rs
@@ -27,29 +27,45 @@ async fn main(_spawner: Spawner) -> ! {
     pins.disable_interrupts(EventType::All);
     pins.clear_interrupts(EventType::All);
 
-    //Blinking LED
-    let mut led = pins.pin10.into_output();
-
     // Configure UART for stdout
     hifive1::stdout::configure(p.UART0, pins.pin17, pins.pin16, 115_200.bps(), clocks);
 
+    sprintln!("Configuring GPIOs...");
+
     // Button pin (GPIO9) as pull-up input
     let mut button = pins.pin9.into_pull_up_input();
-    // Set button interrupt source priority
+    // Configure blue LED pin (GPIO10) as output
+    let mut led = pins.pin10.into_output();
+
+    sprintln!("Configuring external interrupts...");
+
+    // Make sure interrupts are disabled
+    riscv::interrupt::disable();
+
+    // Reset PLIC interrupts and set priority threshold
     let plic = cp.plic;
     let priorities = plic.priorities();
-    priorities.reset::<ExternalInterrupt>();
-    unsafe { button.set_exti_priority(&plic, Priority::P1) };
-
-    // Enable GPIO9 interrupt in PLIC
     let ctx = plic.ctx0();
+    priorities.reset::<ExternalInterrupt>();
     unsafe {
         ctx.enables().disable_all::<ExternalInterrupt>();
         ctx.threshold().set_threshold(Priority::P0);
+    }
+
+    // Enable GPIO9 interrupt for both edges
+    button.enable_interrupt(EventType::BothEdges);
+    unsafe {
+        button.set_exti_priority(&plic, Priority::P1);
         button.enable_exti(&plic);
+    }
+
+    sprintln!("Enabling external interrupts...");
+
+    // Enable global interrupts
+    unsafe {
         riscv::interrupt::enable();
         plic.enable();
-    };
+    }
 
     // Execute loop
     loop {
